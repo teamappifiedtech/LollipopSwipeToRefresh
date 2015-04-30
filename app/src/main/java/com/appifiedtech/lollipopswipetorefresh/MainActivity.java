dd39300c5560fa74306ce2764f046b031b57032b
Priyabrat <teamappifiedtech@gmail.com>
2015-05-01 02:00:51 +0530

    Final code for MainActivity.java

--- app/src/main/java/com/appifiedtech/lollipopswipetorefresh/MainActivity.java
+++ app/src/main/java/com/appifiedtech/lollipopswipetorefresh/MainActivity.java
@@ -1,38 +1,65 @@
 package com.appifiedtech.lollipopswipetorefresh;
 
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+    ListView listView;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    ArrayAdapter<String> adapter;
+    String[] dataArr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.activity_main_listview);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
+        dataArr = getResources().getStringArray(R.array.names_list);
+        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, dataArr);
+        listView.setAdapter(adapter);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new DataFetcher().execute();
+            }
+        });
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.orange,R.color.green,R.color.blue);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    class DataFetcher extends AsyncTask<Void, Void, Void> {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected Void doInBackground(Void... params) {
+            //-- Replace with your server data fetching code here
+            for (int i=0;i<10;i++){
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            //----
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            // Create adapter object and set values for refresh
+            if(mSwipeRefreshLayout.isRefreshing()){
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        }
     }
 }
